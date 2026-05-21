d2e3c0d0b920890f694064d736b6eb7098d80122
Koi no Yokan <70107125+koinoyokan1@users.noreply.github.com>
2026-05-21 22:49:33 +0530

    Add: permutation-string - submission-27

---
+++ Data Structures & Algorithms/permutation-string/submission-27.py
@@ -0,0 +1,19 @@
+'''
+
+'''
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        def isAnagram(s1, s2):
+            s1Cntr = Counter(s1)
+            s2Cntr = Counter(s2)
+            return s1Cntr == s2Cntr
+
+        left, right = 0, len(s1) - 1
+        for right in range(left + len(s1) - 1, len(s2)):
+            if isAnagram(s1, s2[left:right+1]): return True
+            left += 1
+        
+        return False
+
+
+
