2859ae1a4446d0384d1a9d7bb9f73f7522032475
Koi no Yokan <70107125+koinoyokan1@users.noreply.github.com>
2026-05-22 00:08:10 +0530

    Add: string-encode-and-decode - submission-26

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-26.py
@@ -0,0 +1,27 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        encoded = []
+        for s in strs:
+            encoded.append(str(len(s)))
+            encoded.append('|')
+        encoded.append('%')
+        for s in strs:
+            encoded.append(s)
+        print(''.join(encoded))
+        return ''.join(encoded)
+            
+
+    def decode(self, s: str) -> List[str]:
+        meta = s.split('%')[0]
+        lns = meta.split('|')
+        lns = [int(l) for l in lns[:-1]]
+        decoded = []
+
+        st = s.split('%', 1)[1]
+        start = 0
+        for l in lns:
+            decoded.append(st[start:start+l])
+            start += l
+        
+        return decoded
