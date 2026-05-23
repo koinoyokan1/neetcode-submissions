dc54fa53d0a6fb0f2e326f3cd4860705f384d450
Koi no Yokan <70107125+koinoyokan1@users.noreply.github.com>
2026-05-23 17:08:56 +0530

    Add: combination-target-sum - submission-2

---
+++ Data Structures & Algorithms/combination-target-sum/submission-2.py
@@ -0,0 +1,16 @@
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        nums.sort()
+        def _cb(i=0, comb=[], target=target):
+            if target == 0: return [comb]
+            if i == len(nums): return []
+
+            ans = []
+            for j in range(i, len(nums)):
+                if target - nums[j] < 0: break
+                ans.extend(_cb(j, comb + [nums[j]], target - nums[j]))
+            
+            return ans
+        
+        return _cb()
+
