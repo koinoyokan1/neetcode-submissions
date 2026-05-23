f22d8db5ffc027ebea0f66b7cccb577aeeaa5a7a
Koi no Yokan <70107125+koinoyokan1@users.noreply.github.com>
2026-05-23 17:51:51 +0530

    Add: palindromic-substrings - submission-3

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-3.py
@@ -0,0 +1,16 @@
+from functools import lru_cache
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        @lru_cache(maxsize=None)
+        def _hlp(i=0,j=0):
+            if i == j: return True
+
+            if s[i] != s[j]: return False
+            if j == i+1 and s[i] == s[j]: return True
+            return _hlp(i+1,j-1)
+        
+        ans = 0
+        for i in range(len(s)):
+            for j in range(i, len(s)):
+                if _hlp(i,j): ans += 1
+        return ans   
