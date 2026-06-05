ba7f3243ff8c5cde4f1889055b6c0a5455891448
Koi no Yokan <70107125+koinoyokan1@users.noreply.github.com>
2026-06-06 00:57:56 +0530

    Add: first-missing-positive - submission-8

---
+++ Data Structures & Algorithms/first-missing-positive/submission-8.py
@@ -0,0 +1,20 @@
+'''
+n: num exist
+len(nums)+1: invalid
+p
+'''
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        for i in range(len(nums)):
+            if nums[i] <= 0 or nums[i] > len(nums): nums[i] = len(nums) + 1
+
+        for n in nums:
+            n = abs(n)
+            if n == len(nums) + 1: continue
+            
+            nums[n - 1] = -abs(nums[n - 1]) 
+
+        for i in range(len(nums)):
+            if nums[i] >= 0: return i+1  
+
+        return len(nums) + 1
