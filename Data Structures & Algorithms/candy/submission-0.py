48ac7f47112b3a79e4789465b5094b4f439ecc84
Koi no Yokan <70107125+koinoyokan1@users.noreply.github.com>
2026-06-06 01:22:13 +0530

    Add: candy - submission-0

---
+++ Data Structures & Algorithms/candy/submission-0.py
@@ -0,0 +1,18 @@
+from functools import lru_cache
+
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+
+        @lru_cache(None)
+        def assignCandies(i):
+            ans = 1
+
+            if i > 0 and ratings[i] > ratings[i - 1]:
+                ans = max(ans, assignCandies(i - 1) + 1)
+
+            if i < len(ratings) - 1 and ratings[i] > ratings[i + 1]:
+                ans = max(ans, assignCandies(i + 1) + 1)
+
+            return ans
+
+        return sum(assignCandies(i) for i in range(len(ratings)))
