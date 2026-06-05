31aa8da5994dae5ede13abfcb3cbd98b6748ce99
Koi no Yokan <70107125+koinoyokan1@users.noreply.github.com>
2026-06-06 00:25:07 +0530

    Add: dota2-senate - submission-9

---
+++ Data Structures & Algorithms/dota2-senate/submission-9.py
@@ -0,0 +1,22 @@
+class Solution:
+    def predictPartyVictory(self, senate: str) -> str:
+        d, r = deque(), deque()
+
+        for i in range(len(senate)):
+            s = senate[i]
+            if s == 'D': d.append(i)
+            else: r.append(i)
+        
+        while d and r:
+            prev = max(d[-1], r[-1]) + 1
+            if d[0] > r[0]:
+                _ = d.popleft()
+                v = r.popleft()
+                r.append(prev)
+            else:
+                v = d.popleft()
+                _ = r.popleft()
+                d.append(prev)
+
+        if d: return 'Dire'
+        return 'Radiant'
