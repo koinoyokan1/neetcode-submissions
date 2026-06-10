50554a0859aa66ace62a0ba4edef63397b4701b6
Koi no Yokan <70107125+koinoyokan1@users.noreply.github.com>
2026-06-11 01:59:03 +0530

    Add: open-the-lock - submission-3

---
+++ Data Structures & Algorithms/open-the-lock/submission-3.py
@@ -0,0 +1,24 @@
+class Solution:
+    def openLock(self, deadends: List[str], target: str) -> int:
+        if '0000' in deadends: return -1
+        cnt = 0
+        start = (0,0,0,0)
+        queue = deque([(cnt, start)])
+        visited = set()
+
+        while queue:
+            cnt, lock = queue.popleft()
+            if lock in visited: continue
+            visited.add(lock)
+            if ''.join(map(str, lock)) == target: return cnt 
+            lock = list(lock)
+            for i in range(4):
+                for j in [-1, +1]:
+                    lock[i] = (lock[i] + j) % 10
+                    if ''.join(map(str, lock)) in deadends: 
+                        lock[i] = (lock[i] - j) % 10
+                        continue 
+                    queue.append((cnt+1, tuple(lock)))
+                    lock[i] = (lock[i] - j) % 10
+        
+        return -1
