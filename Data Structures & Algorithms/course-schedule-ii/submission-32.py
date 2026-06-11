41d97f0efa5c6b704d27c58ea7363eaa00e4ee3c
Koi no Yokan <70107125+koinoyokan1@users.noreply.github.com>
2026-06-11 22:33:28 +0530

    Add: course-schedule-ii - submission-32

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-32.py
@@ -0,0 +1,34 @@
+from collections import defaultdict
+
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        ans = []
+        graph = defaultdict(list)
+
+        for p in prerequisites:
+            graph[p[0]].append(p[1])
+        
+        UNVISITED, VISITING, VISITED = 0, 1, 2 
+        status = [UNVISITED] * numCourses
+        ans = []
+
+        def dfs(n):
+            if status[n] == VISITED: return True
+            if status[n] == VISITING: return False
+
+            status[n] = VISITING
+
+            for nei in graph[n]:
+                if not dfs(nei): return False
+
+            status[n] = VISITED
+            ans.append(n)
+
+            return True
+
+        for i in range(numCourses):
+            if status[i] == VISITED: continue
+
+            if not dfs(i): return []
+        
+        return ans
