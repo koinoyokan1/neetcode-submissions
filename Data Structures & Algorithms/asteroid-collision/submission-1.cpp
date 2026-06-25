cb33cad52c9752accad17de9b16fa3362260a7e4
Koi no Yokan <70107125+koinoyokan1@users.noreply.github.com>
2026-06-25 17:19:47 +0530

    Add: asteroid-collision - submission-1

---
+++ Data Structures & Algorithms/asteroid-collision/submission-1.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        stack<int> st;
+
+        for (int a:asteroids) {
+            if (a > 0) {
+                st.push(a);
+                continue;
+            }
+            bool addA = true;
+            while (st.size() != 0 && st.top() > 0) {
+                int b = st.top();
+
+                if (abs(a) == b) {
+                    st.pop();
+                    addA = false;
+                    break;
+                } else if (abs(a) < b) {
+                    addA = false;
+                    break;
+                } else {
+                    st.pop();
+                }
+            }
+            if (addA) {
+                st.push(a);
+            }
+        }
+        vector<int> ans;
+        while(!st.empty()) {
+            int x = st.top();
+            ans.push_back(x);
+            st.pop();
+        } 
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
