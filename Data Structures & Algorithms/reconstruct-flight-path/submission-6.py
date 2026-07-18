8edfe1e24be394ba98c277d97672ca8f09f9a9bf
Koi no Yokan <70107125+koinoyokan1@users.noreply.github.com>
2026-07-18 19:19:04 +0530

    Add: reconstruct-flight-path - submission-6

---
+++ Data Structures & Algorithms/reconstruct-flight-path/submission-6.py
@@ -0,0 +1,34 @@
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        tickets.sort()
+        graph = defaultdict(list)
+
+        for frm, to in tickets:
+            graph[frm].append(to)
+        
+        ans = ["JFK"]
+        flightsTaken = 0
+
+        def dfs(frm):
+            nonlocal flightsTaken
+
+            if flightsTaken == len(tickets):
+                return True
+
+            for i in range(len(graph[frm])):
+                to = graph[frm][i]
+                graph[frm].pop(i)
+                flightsTaken += 1
+                ans.append(to)
+
+                if dfs(to):
+                    return True
+
+                ans.pop()
+                flightsTaken -= 1
+                graph[frm].insert(i, to)
+
+            return False
+
+        dfs("JFK")
+        return ans
